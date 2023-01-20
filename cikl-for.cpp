c27f3fb396fdd3a97e31d52a2ae2eb6916f72cb0
Ladr00 <52217415+Ladr00@users.noreply.github.com>
2023-01-20 09:29:39 +0300

    Update cikl-for.cpp

--- cikl-for.cpp
+++ cikl-for.cpp
@@ -2,13 +2,13 @@
 
 main()
 {
-	int i = 1;
+    int i = 1;
     int sum = 0;
     for (;i < 100; i = i + 1)
     
     {
         sum = sum + i;
-		printf("%i+", i);
+	printf("%i+", i);
     }
     
     sum = sum + i;
